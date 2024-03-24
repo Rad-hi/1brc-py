d005f7a7fe249e4b5c76e7f381a68aad001a9b1b
Rad-hi <radhisghaier33@gmail.com>
2024-03-25 00:48:23 +0100

    who would have thought that the standard library is faster than my stupid code

--- test.py
+++ test.py
@@ -6,9 +6,17 @@ def test_str2f():
         '12.2\n',
         '-2.9\n',
         '2.9\n',
+        '-12.2',
+        '12.2',
+        '-2.9',
+        '2.9',
     ]
 
     gts = [
+        -12.2,
+        12.2,
+        -2.9,
+        2.9,
         -12.2,
         12.2,
         -2.9,
@@ -18,3 +26,18 @@ def test_str2f():
     for in_, gt in zip(ins, gts):
         out = str2f(in_)
         assert out == gt
+
+
+import time
+
+in_ = '-12.5'
+
+ts = time.perf_counter()
+str2f(in_)
+tss = time.perf_counter() - ts
+print(f'{tss * 1000_000.0} us')
+
+ts = time.perf_counter()
+float(in_)
+tss = time.perf_counter() - ts
+print(f'{tss * 1000_000.0} us')
